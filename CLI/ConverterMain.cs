96f16be02e00fa517dd0ed8ea01954432dc29a41
Housey <housebreaker1@gmail.com>
2016-03-18 19:01:46 +0200

    move into CLI folder (getting ready for GUI version)

---
+++ CLI/ConverterMain.cs
@@ -0,0 +1,12 @@
+﻿namespace BCSTM_to_Wav_Converter
+{
+	using System;
+
+	public static class ConverterMain
+	{
+		public static void Main(string[] args)
+		{
+			BCSTMConverter.ConvertToWav(args);
+		}
+	}
+}
